2ede47b15d1c3f421bb30be60dba96c7c294150b
martirosyand <dmartirosyan@griddynamics.com>
2026-06-01 21:21:38 +0400

    Remove admin button from navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -33,11 +33,11 @@ const Navbar = () => {
             <li><Link to="/inventory" onClick={closeMenu} className={location.pathname === '/inventory' ? 'active-link' : ''}>VÉHICULES</Link></li>
             <li><Link to="/services" onClick={closeMenu} className={location.pathname === '/services' ? 'active-link' : ''}>NOS PRESTATIONS</Link></li>
             <li><Link to="/contact" onClick={closeMenu} className={location.pathname === '/contact' ? 'active-link' : ''}>CONTACT</Link></li>
-            <li>
+            {/* <li>
               <Link to="/admin" onClick={closeMenu} className={`admin-link ${location.pathname.startsWith('/admin') ? 'active-link' : ''}`}>
                 <User size={18} /> Navigation privée
               </Link>
-            </li>
+            </li> */}
           </ul>
         </nav>
       </div>
